8de1ef84d7f88554eeb01c8d2c03a637ebcd753c
Xamza <125999650+copypaste11@users.noreply.github.com>
2024-04-11 18:15:47 +0500

    updated challenge.c and created binary

---
+++ challenges24-04/challenges/challenge00.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char specific_flag[] = "PM{Haa9_B6sT_C6nT6R}";
+    char entered_flag[100];
+
+    // Prompt user to enter the password
+    printf("Please enter the flag: ");
+    scanf("%s", entered_flag);
+
+    // Check if the entered password matches the specific password
+    if (strcmp(entered_flag, specific_flag) == 0) {
+        printf("Congratulations! Here is the flag: PW{N0th1ng_To_D0_f0r_CTF}\n");
+    } else {
+        printf("Incorrect password. Try again.\n");
+    }
+
+    return 0;
+}
