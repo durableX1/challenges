cf50a1ef1f1de73e18a26819a148c21aadecdb03
Xamza <125999650+copypaste11@users.noreply.github.com>
2024-04-12 15:30:48 +0500

    other updates

--- challenges24-04/challenges/challenge04.c
+++ challenges24-04/challenges/challenge04.c
@@ -2,15 +2,15 @@
 #include <string.h>
 
 int main() {
-    char specific_flag[] = "PY{G00d_Th@T_u_c@nnotrun_binary}";
-    char entered_flag[100];
+    char specific_password[] = "VM{Tr@nsper@ncy_1s_W0rking}";
+    char entered_password[100];
 
     // Prompt user to enter the password
     printf("Please enter the passowrd: ");
-    scanf("%s", entered_flag);
+    scanf("%s", entered_password);
 
     // Check if the entered password matches the specific password
-    if (strcmp(entered_flag, specific_flag) == 0) {
+    if (strcmp(entered_password, specific_password) == 0) {
         printf("\nIn *barbican* directory you Have Read a Files and Follow the Directories I Think You found your Flag!");
     } else {
         printf("Incorrect password. Try again!!!\n");
